e359bbece17a2e43f503eca152359e9301294990
chlee <darthegg21@gmail.com>
2022-06-23 17:51:51 +0900

    Implement video-like path

--- run_video_display.py
+++ run_video_display.py
@@ -35,6 +35,8 @@ if __name__ == "__main__":
     semantic_sensor = False
 
     meters_per_pixel = 0.1
+    translation_threshold = 0.5
+    interpolation_interval = 0.1
 
     sim_settings = {
         "width": 256,  # Spatial resolution of the observations
@@ -68,122 +70,64 @@ if __name__ == "__main__":
 
     recolored_topdown_map_list = get_entire_maps_by_levels(sim, meters_per_pixel)
 
-    # while True:
-    #     sample1 = sim.pathfinder.get_random_navigable_point()
-    #     sample2 = sim.pathfinder.get_random_navigable_point()
+    ext_trans_mat_list = pose_trace["extrinsic_matrix"]
+    trans_mat = ext_trans_mat_list[0]
+    position, _ = convert_transmat_to_point_quaternion(trans_mat)
 
-    #     path = habitat_sim.ShortestPath()
-    #     path.requested_start = sample1
-    #     path.requested_end = sample2
-    #     found_path = sim.pathfinder.find_path(path)
-    #     geodesic_distance = path.geodesic_distance
-    #     path_points = path.points
-    #     print(path_points)
-    #     input()
-    #     if path_points == []:
-    #         continue
-    #     else:
-    #         break
-
-    # recolored_topdown_map = get_closest_map(sim, sample1, recolored_topdown_map_list)
-
-    # sample1_point = maps.to_grid(sample1[2], sample1[0], recolored_topdown_map.shape[0:2], sim)
-    # transposed_point = (sample1_point[1], sample1_point[0])
-    # cv2.drawMarker(
-    #     img=recolored_topdown_map,
-    #     position=(int(transposed_point[0]), int(transposed_point[1])),
-    #     color=(0, 255, 0),
-    #     markerType=cv2.MARKER_DIAMOND,
-    #     markerSize=2,
-    # )
-
-    # sample2_point = maps.to_grid(sample2[2], sample2[0], recolored_topdown_map.shape[0:2], sim)
-    # transposed_point = (sample2_point[1], sample2_point[0])
-    # cv2.drawMarker(
-    #     img=recolored_topdown_map,
-    #     position=(int(transposed_point[0]), int(transposed_point[1])),
-    #     color=(0, 255, 0),
-    #     markerType=cv2.MARKER_DIAMOND,
-    #     markerSize=2,
-    # )
-
-    # nodes = []
-    # for position in path_points:
-    #     agent_state.position = position
-    #     print(position)
-    #     agent.set_state(agent_state)
-    #     observations = sim.get_sensor_observations()
-    #     color_img = cv2.cvtColor(observations["color_sensor"], cv2.COLOR_BGR2RGB)
-    #     key = display_opencv_cam(color_img)
-
-    #     node_point = maps.to_grid(position[2], position[0], recolored_topdown_map.shape[0:2], sim)
-    #     transposed_point = (node_point[1], node_point[0])
-    #     nodes.append(transposed_point)
-    #     display_map(recolored_topdown_map, nodes)
+    recolored_topdown_map = get_closest_map(sim, position, recolored_topdown_map_list)
 
     # Option 1
     # TODO: Just use dataset from pose diff
     # TODO: check the number of data (especially orientation)
     # TODO: Only the amount of translation can be varied
     # TODO: Path interpolation must be implemented for translation variation
-    # TODO: How to discriminate rotation and translation
     # TODO: Backward translation...?
 
-    # Option 2
-    # TODO: Implement size-variable action
+    pos_trajectory = []
+    angle_trajectory = []
 
-    ext_trans_mat_list = pose_trace["extrinsic_matrix"]
-    trans_mat = ext_trans_mat_list[0]
-    position, _ = convert_transmat_to_point_quaternion(trans_mat)
+    for trans_mat in ext_trans_mat_list:
+        position, angle_quaternion = convert_transmat_to_point_quaternion(trans_mat)
+        pos_trajectory.append(position)
+        angle_trajectory.append(angle_quaternion)
 
-    print(len(ext_trans_mat_list))
-    input()
+    for i, position in enumerate(pos_trajectory):
+        if i + 1 < len(pos_trajectory):
+            next_position = pos_trajectory[i + 1]
+            next_angle = angle_trajectory[i + 1]
 
-    # for i in range(0, len(ext_trans_mat_list), 100):
-    #     position, angle_quaternion = convert_transmat_to_point_quaternion(ext_trans_mat_list[i])
-    #     if (i + 100 <= len(ext_trans_mat_list)):
-    #         next_pos, next_angle = convert_transmat_to_point_quaternion(ext_trans_mat_list[i + 100])
-    #     print("current pos: ", position, angle_quaternion)
-    #     print("next pos: ", next_pos, next_angle)
-    #     print("pos diff: ", next_pos - position)
-    #     print("angle diff: ", next_angle - angle_quaternion)
-    #     print(i)
-    #     input()
+        dist = np.sqrt(np.sum((next_position - position) ** 2, axis=0))
+        num_interpolation = int(dist // interpolation_interval)
 
-    # for i, trans_mat in enumerate(ext_trans_mat_list):
-    #     position, angle_quaternion = convert_transmat_to_point_quaternion(trans_mat)
-    #     if (i + 1 <= len(ext_trans_mat_list)):
-    #         next_pos, next_angle = convert_transmat_to_point_quaternion(ext_trans_mat_list[i + 1])
-    #     print("current pos: ", position, angle_quaternion)
-    #     print("pos diff: ", next_pos - position)
-    #     print("angle diff: ", next_angle - angle_quaternion)
-    #     print(i)
-    #     input()
-
-    recolored_topdown_map = get_closest_map(sim, position, recolored_topdown_map_list)
+        interval_pos_list = []
+        if dist > translation_threshold:
+            interval_pos_list = np.linspace(position, next_position, num=num_interpolation)
+            for n, interval_pos in enumerate(interval_pos_list):
+                pos_trajectory.insert(i + n + 1, interval_pos)
+                angle_trajectory.insert(i + n + 1, angle_trajectory[i])
 
     img_id = 0
-    while True:
-        nodes = []
-        for i in range(0, len(ext_trans_mat_list), 100):
-            trans_mat = ext_trans_mat_list[i]
-            position, angle_quaternion = convert_transmat_to_point_quaternion(trans_mat)
-            agent_state.position = position
-            print(position)
-            agent_state.rotation = angle_quaternion
-            agent.set_state(agent_state)
-            observations = sim.get_sensor_observations()
-            color_img = cv2.cvtColor(observations["color_sensor"], cv2.COLOR_BGR2RGB)
-            key = display_opencv_cam(color_img)
+    nodes = []
 
-            if key == ord("o"):
-                print("save image")
-                cv2.imwrite(f"./output/query{img_id}.jpg", color_img)
-                cv2.imwrite(f"./output/db{img_id}.jpg", color_img)
-                img_id = img_id + 1
-                continue
+    for i in range(0, len(pos_trajectory), 1):
+        position = pos_trajectory[i]
+        angle_quaternion = angle_trajectory[i]
+        agent_state.position = position
+        print("Frame: ", i, "Position: ", position)
+        agent_state.rotation = angle_quaternion
+        agent.set_state(agent_state)
+        observations = sim.get_sensor_observations()
+        color_img = cv2.cvtColor(observations["color_sensor"], cv2.COLOR_BGR2RGB)
+        key = display_opencv_cam(color_img)
 
-            node_point = maps.to_grid(position[2], position[0], recolored_topdown_map.shape[0:2], sim)
-            transposed_point = (node_point[1], node_point[0])
-            nodes.append(transposed_point)
-            display_map(recolored_topdown_map, nodes)
+        if key == ord("o"):
+            print("save image")
+            cv2.imwrite(f"./output/query{img_id}.jpg", color_img)
+            cv2.imwrite(f"./output/db{img_id}.jpg", color_img)
+            img_id = img_id + 1
+            continue
+
+        node_point = maps.to_grid(position[2], position[0], recolored_topdown_map.shape[0:2], sim)
+        transposed_point = (node_point[1], node_point[0])
+        nodes.append(transposed_point)
+        display_map(recolored_topdown_map, nodes)
