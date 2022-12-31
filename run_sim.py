3c1e390e1c4d6dc4fca50f358b01e34fe88c4d86
chlee <darthegg21@gmail.com>
2023-01-01 00:46:04 +0900

    Fix initial agent position to be sampled on topdown map grey area

--- run_sim.py
+++ run_sim.py
@@ -1,9 +1,11 @@
 import argparse
 import json
 import os
+import random
 
 import cv2
 from habitat.utils.visualizations import maps
+import numpy as np
 
 from config.env_config import ActionConfig, CamFourViewConfig, PathConfig
 from habitat_env.environment import HabitatSimWithMap
@@ -66,124 +68,135 @@ if __name__ == "__main__":
 
     for scene_number in scene_list:
         sim = HabitatSimWithMap(scene_number, CamFourViewConfig, ActionConfig, PathConfig, height_data, is_detection)
-        observation_path, pos_record_json = make_output_path(output_path, scene_number)
 
-        img_id = 0
-        pos_record = {}
-        pos_record.update({"scene_number": scene_number})
+        for level, recolored_topdown_map in enumerate(sim.recolored_topdown_map_list):
+            print("scene: ", scene_number, "    level: ", level)
+            observation_path, pos_record_json = make_output_path(output_path, scene_number)
 
-        if is_localization:
-            current_state = sim.agent.get_state()
-            position = current_state.position
-            sim.update_closest_map(position)
+            img_id = 0
+            pos_record = {}
+            pos_record.update({"scene_number": scene_number})
+            
+            # Sample initial agent position from topdown map grey area
+            # This is for fixing height of agent position
+            topdown_map = sim.topdown_map_list[level]
+            explorable_area_index = list(zip(*np.where(topdown_map == 1)))
+            grid_pos = random.sample(explorable_area_index, 1)[0]
+            sim.set_state_from_grid(grid_pos, level)
 
-            # Read binary topdown map
-            binary_topdown_map = sim.topdown_map_list[sim.closest_level]
-            # Set file path
-            current_map_dir = os.path.join(
-                map_obs_path, f"observation_{scene_number}", f"map_node_observation_level_{sim.closest_level}"
-            )
-            # Initialize localization instance
-            localization = Localization(top_network, bottom_network, binary_topdown_map, current_map_dir)
-
-        # Initialize opencv display window
-        init_map_display()
-        init_opencv_cam()
-
-        while True:
-            # Get camera observation
-            observations = sim.get_cam_observations()
-            color_img = observations["all_view"]
-
-            # Get current position
-            current_state = sim.agent.get_state()
-            position = current_state.position
-
-            # Update map data
-            previous_level = sim.closest_level
-            sim.update_closest_map(position)
-            map_image = cv2.cvtColor(sim.recolored_topdown_map, cv2.COLOR_GRAY2BGR)
-
-            # If level is changed or graph is not initialized, build graph
-            current_level = sim.closest_level
-            if previous_level != current_level and is_localization:
-                current_map_dir = os.path.join(
-                    map_obs_path, f"observation_{scene_number}", f"map_node_observation_level_{current_level}"
-                )
-                localization = Localization(
-                    top_network, bottom_network, sim.topdown_map_list[current_level], current_map_dir
-                )
-
-            # Execute localization
             if is_localization:
-                obs_embedding = localization.calculate_embedding_from_observation(color_img)
-                localization_result = localization.localize_with_observation(obs_embedding)
-                map_image = localization.visualize_on_map(map_image, localization_result)
+                current_state = sim.agent.get_state()
+                position = current_state.position
+                sim.update_closest_map(position)
 
-            node_point = maps.to_grid(position[2], position[0], sim.recolored_topdown_map.shape[0:2], sim)
-            display_map(map_image, key_points=[node_point])
+                # Read binary topdown map
+                binary_topdown_map = sim.topdown_map_list[sim.closest_level]
+                # Set file path
+                current_map_dir = os.path.join(
+                    map_obs_path, f"observation_{scene_number}", f"map_node_observation_level_{sim.closest_level}"
+                )
+                # Initialize localization instance
+                localization = Localization(top_network, bottom_network, binary_topdown_map, current_map_dir)
 
-            # Display observation. If YOLO is available, display object detection result
-            if is_detection:
-                detect_img = sim.detect_img(observations)
-                key = display_opencv_cam(detect_img)
-            else:
-                key = display_opencv_cam(color_img)
+            # Initialize opencv display window
+            init_map_display()
+            init_opencv_cam()
 
-            # Set action according to key input
-            if key == ord("w"):
-                action = "move_forward"
-            if key == ord("s"):
-                action = "move_backward"
-            if key == ord("a"):
-                action = "turn_left"
-            if key == ord("d"):
-                action = "turn_right"
-            if key == ord("n"):
-                break
-            if key == ord("q"):
-                break
+            while True:
+                # Get camera observation
+                observations = sim.get_cam_observations()
+                color_img = observations["all_view"]
 
-            # Save observation & position record according to the flag
-            # Save observation every step
-            if is_save_all:
-                cv2.imwrite(observation_path + os.sep + f"{img_id:06d}.jpg", color_img)
-                sim_pos = {f"{img_id:06d}_sim": [float(pos) for pos in position]}
-                grid_pos = {f"{img_id:06d}_grid": [int(pnt) for pnt in node_point]}
-                pos_record.update(sim_pos)
-                pos_record.update(grid_pos)
-                img_id = img_id + 1
-            # Save observation only when forward & backward movement
-            if is_save_except_rotation:
-                if key == ord("w") or key == ord("s"):
-                    cv2.imwrite(observation_path + os.sep + f"{img_id:06d}.jpg", color_img)
-                    sim_pos = {f"{img_id:06d}_sim": [float(pos) for pos in position]}
-                    grid_pos = {f"{img_id:06d}_grid": [int(pnt) for pnt in node_point]}
-                    pos_record.update(sim_pos)
-                    pos_record.update(grid_pos)
-                    img_id = img_id + 1
-            # Save observation when "o" key input
-            if key == ord("o"):
-                if is_save_all or is_save_except_rotation:
-                    pass
+                # Get current position
+                current_state = sim.agent.get_state()
+                position = current_state.position
+                print("current height: ", position[1], "height list value: ", sim.height_list[level])
+
+                # Update map data
+                previous_level = sim.closest_level
+                sim.update_closest_map(position)
+                map_image = cv2.cvtColor(sim.recolored_topdown_map, cv2.COLOR_GRAY2BGR)
+
+                # If level is changed or graph is not initialized, build graph
+                current_level = sim.closest_level
+                if previous_level != current_level and is_localization:
+                    current_map_dir = os.path.join(
+                        map_obs_path, f"observation_{scene_number}", f"map_node_observation_level_{current_level}"
+                    )
+                    localization = Localization(
+                        top_network, bottom_network, sim.topdown_map_list[current_level], current_map_dir
+                    )
+
+                # Execute localization
+                if is_localization:
+                    obs_embedding = localization.calculate_embedding_from_observation(color_img)
+                    localization_result = localization.localize_with_observation(obs_embedding)
+                    map_image = localization.visualize_on_map(map_image, localization_result)
+
+                node_point = maps.to_grid(position[2], position[0], sim.recolored_topdown_map.shape[0:2], sim)
+                display_map(map_image, key_points=[node_point])
+
+                # Display observation. If YOLO is available, display object detection result
+                if is_detection:
+                    detect_img = sim.detect_img(observations)
+                    key = display_opencv_cam(detect_img)
                 else:
-                    print("save image")
+                    key = display_opencv_cam(color_img)
+
+                # Set action according to key input
+                if key == ord("w"):
+                    action = "move_forward"
+                if key == ord("s"):
+                    action = "move_backward"
+                if key == ord("a"):
+                    action = "turn_left"
+                if key == ord("d"):
+                    action = "turn_right"
+                if key == ord("n"):
+                    break
+                if key == ord("q"):
+                    break
+
+                # Save observation & position record according to the flag
+                # Save observation every step
+                if is_save_all:
                     cv2.imwrite(observation_path + os.sep + f"{img_id:06d}.jpg", color_img)
                     sim_pos = {f"{img_id:06d}_sim": [float(pos) for pos in position]}
                     grid_pos = {f"{img_id:06d}_grid": [int(pnt) for pnt in node_point]}
                     pos_record.update(sim_pos)
                     pos_record.update(grid_pos)
                     img_id = img_id + 1
-                    continue
+                # Save observation only when forward & backward movement
+                if is_save_except_rotation:
+                    if key == ord("w") or key == ord("s"):
+                        cv2.imwrite(observation_path + os.sep + f"{img_id:06d}.jpg", color_img)
+                        sim_pos = {f"{img_id:06d}_sim": [float(pos) for pos in position]}
+                        grid_pos = {f"{img_id:06d}_grid": [int(pnt) for pnt in node_point]}
+                        pos_record.update(sim_pos)
+                        pos_record.update(grid_pos)
+                        img_id = img_id + 1
+                # Save observation when "o" key input
+                if key == ord("o"):
+                    if is_save_all or is_save_except_rotation:
+                        pass
+                    else:
+                        print("save image")
+                        cv2.imwrite(observation_path + os.sep + f"{img_id:06d}.jpg", color_img)
+                        sim_pos = {f"{img_id:06d}_sim": [float(pos) for pos in position]}
+                        grid_pos = {f"{img_id:06d}_grid": [int(pnt) for pnt in node_point]}
+                        pos_record.update(sim_pos)
+                        pos_record.update(grid_pos)
+                        img_id = img_id + 1
+                        continue
 
-            sim.step(action)
+                sim.step(action)
 
-        file_saved = os.listdir(observation_path)
-        if is_save_all or is_save_except_rotation or file_saved:
-            with open(pos_record_json, "w") as record_json:  # pylint: disable=unspecified-encoding
-                json.dump(pos_record, record_json, indent=4)
-        else:
-            os.rmdir(observation_path)
+            file_saved = os.listdir(observation_path)
+            if is_save_all or is_save_except_rotation or file_saved:
+                with open(pos_record_json, "w") as record_json:  # pylint: disable=unspecified-encoding
+                    json.dump(pos_record, record_json, indent=4)
+            else:
+                os.rmdir(observation_path)
 
         sim.close()
 
