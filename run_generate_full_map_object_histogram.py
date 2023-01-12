b39028623c49eb3dc380a4970c8cd6e33706f25a
chlee <darthegg21@gmail.com>
2023-01-12 10:04:58 +0900

    Add save result in object histogram generation

--- run_generate_full_map_object_histogram.py
+++ run_generate_full_map_object_histogram.py
@@ -8,7 +8,7 @@ from algorithms.yolo import Yolo
 from config.env_config import ActionConfig, CamFourViewConfig, PathConfig
 from habitat_env.environment import HabitatSimWithMap
 from habitat_env.object_spatial_pyramid import ObjectSpatialPyramid
-from utils.habitat_utils import open_env_related_files
+from utils.habitat_utils import display_opencv_cam, init_opencv_cam, open_env_related_files
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -21,9 +21,6 @@ if __name__ == "__main__":
     scene_index = args.scene_index
     height_json_path = args.map_height_json
     map_obs_path = args.map_obs_path
-
-    total_list_to_iterate = []
-    output_size_list = []
 
     # Open files
     scene_list, height_data = open_env_related_files(scene_list_file, height_json_path, scene_index)
@@ -42,7 +39,7 @@ if __name__ == "__main__":
         if num_level == 0:
             raise ValueError("Height data is not found.")
 
-        for level, recolored_topdown_map in enumerate(sim.recolored_topdown_map_list):
+        for level in range(num_level):
             print("scene: ", scene_number, "    level: ", level)
             map_obs_dir = os.path.join(observation_path, f"map_node_observation_level_{level}")
             sample_dir = os.path.join(observation_path, f"test_sample_{level}")
@@ -59,9 +56,11 @@ if __name__ == "__main__":
             sample_list = [sample_dir + os.sep + file for file in sorted_test_sample_file]
 
             # Initialize output matrix
-            shape_histogram = object_spatial_pyramid.num_support * 21
+            shape_histogram = object_spatial_pyramid.num_support * (1 + 4 + 16)
             map_output_array = np.zeros([len(map_obs_list), shape_histogram])
             sample_output_array = np.zeros([len(sample_list), shape_histogram])
+
+            init_opencv_cam()
 
             for i, map_obs in enumerate(map_obs_list):
                 img = cv2.imread(map_obs)
@@ -78,7 +77,32 @@ if __name__ == "__main__":
                 cam_observations["back_view"] = img[:, 512:768]
                 cam_observations["left_view"] = img[:, 768:1024]
 
+                detect_img, detection_result = sim.detect_img(cam_observations, yolo)
+                display_opencv_cam(detect_img)
+                histogram = object_spatial_pyramid.make_spatial_histogram(detection_result)
+                map_output_array[i] = histogram
+
+            for i, sample_obs in enumerate(sample_list):
+                img = cv2.imread(sample_obs)
+                cam_observations = {
+                    "all_view": None,
+                    "front_view": None,
+                    "right_view": None,
+                    "back_view": None,
+                    "left_view": None,
+                }
+
+                cam_observations["front_view"] = img[:, 0:256]
+                cam_observations["right_view"] = img[:, 256:512]
+                cam_observations["back_view"] = img[:, 512:768]
+                cam_observations["left_view"] = img[:, 768:1024]
+
                 _, detection_result = sim.detect_img(cam_observations, yolo)
                 histogram = object_spatial_pyramid.make_spatial_histogram(detection_result)
+                sample_output_array[i] = histogram
 
-                map_output_array[i] = histogram
+            with open(map_output, "wb") as f:
+                np.save(f, map_output_array)
+
+            with open(sample_output, "wb") as f:
+                np.save(f, sample_output_array)
