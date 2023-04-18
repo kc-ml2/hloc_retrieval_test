c7b1d2d8ee55f865fb048903c0596c9c849fb5cb
chlee <darthegg21@gmail.com>
2023-04-18 20:04:42 +0900

    Fix color palette error in room segmentation

--- room_segmentation/main.cpp
+++ room_segmentation/main.cpp
@@ -1,3 +1,62 @@
+/*!
+ *****************************************************************
+ * \file
+ *
+ * \note
+ * Copyright (c) 2015 \n
+ * Fraunhofer Institute for Manufacturing Engineering
+ * and Automation (IPA) \n\n
+ *
+ *****************************************************************
+ *
+ * \note
+ * Project name: Care-O-bot
+ * \note
+ * ROS stack name: autopnp
+ * \note
+ * ROS package name: ipa_room_segmentation
+ *
+ * \author
+ * Author: Florian Jordan
+ * \author
+ * Supervised by: Richard Bormann
+ *
+ * \date Date of creation: 08.2015
+ *
+ * \brief
+ *
+ *
+ *****************************************************************
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer. \n
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution. \n
+ * - Neither the name of the Fraunhofer Institute for Manufacturing
+ * Engineering and Automation (IPA) nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission. \n
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License LGPL for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License LGPL along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************/
+
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -16,15 +75,32 @@ static bool DEBUG_DISPLAYS=false;
 
 int main(int argc, char** argv)
 {
-	cv::Mat original_img;
+	std::string image_path = cv::samples::findFile("./../../data/recolored_topdown/1LXtFkjw3qL_0.bmp");
+    cv::Mat original_img = cv::imread(image_path, cv::IMREAD_COLOR);
 
-	const float map_resolution = 0.05;
+	cv::uint8_t white_val = 255;
+	cv::uint8_t black_val = 0;
+
+	for (int j = 0; j < original_img.cols; j++) {
+		for (int i = 0; i < original_img.rows; i++) {
+            if (original_img.at<cv::Vec3b>(i, j) == cv::Vec3b(255, 255, 255))
+                original_img.at<cv::Vec3b>(i, j) = cv::Vec3b(black_val, black_val, black_val);
+            if (original_img.at<cv::Vec3b>(i, j) == cv::Vec3b(128, 128, 128))
+                original_img.at<cv::Vec3b>(i, j) = cv::Vec3b(white_val, white_val, white_val);
+		}
+	}
+
+	// original_img.convertTo(original_img, CV_8UC1);
+	cv::Mat img;
+	cv::cvtColor(original_img, img, cv::COLOR_BGR2GRAY);
+
+	const float map_resolution = 0.1;
 	const cv::Point2d map_origin(const float map_origin_position_x=0.0, const float map_origin_position_y=0.0);
     const double robot_radius = 0.3;
     double room_lower_limit_voronoi_ = 0.1;	//1.53;
 	double room_upper_limit_voronoi_ = 1000000.;	//120.0;
 	int voronoi_neighborhood_index_ = 280;
-	int max_iterations_ = 150;
+	int max_iterations_ = 16;
 	double min_critical_point_distance_factor_ = 0.5; //1.6;
 	double max_area_for_merging_ = 12.5;
 	bool display_segmented_map_ = true;
@@ -33,7 +109,7 @@ int main(int argc, char** argv)
 	//segment the given map
 	cv::Mat segmented_map;
 	VoronoiSegmentation voronoi_segmentation; //voronoi segmentation method
-	voronoi_segmentation.segmentMap(original_img, segmented_map, map_resolution, room_lower_limit_voronoi_, room_upper_limit_voronoi_,
+	voronoi_segmentation.segmentMap(img, segmented_map, map_resolution, room_lower_limit_voronoi_, room_upper_limit_voronoi_,
 		voronoi_neighborhood_index_, max_iterations_, min_critical_point_distance_factor_, max_area_for_merging_, (display_segmented_map_&&DEBUG_DISPLAYS));
 
 	// get the min/max-values and the room-centers
@@ -221,6 +297,8 @@ int main(int argc, char** argv)
 		for (size_t index = 0; index < room_centers_x_values.size(); ++index)
 			cv::circle(color_segmented_map, cv::Point(room_centers_x_values[index], room_centers_y_values[index]), 2, cv::Scalar(256), cv::FILLED);
 
+		cv::namedWindow("segmentation", cv::WINDOW_NORMAL);
+		cv::resizeWindow("segmentation", 1000, 1000);
 		cv::imshow("segmentation", color_segmented_map);
 		cv::waitKey();
 	}
