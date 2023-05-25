cf0b8e042ba7d015cee39bdb78dae34127218a17
chlee <darthegg21@gmail.com>
2023-05-25 11:21:30 +0900

    Add comment in ORB matching

--- relocalization/localization_orb_matching.py
+++ relocalization/localization_orb_matching.py
@@ -8,7 +8,10 @@ from relocalization.localization_base import LocalizationBase
 
 
 class LocalizationOrbMatching(LocalizationBase):
-    """Class for global localization methods with ORB local feature."""
+    """Class for global localization methods with ORB local feature.
+        This class does not use DBOW(Bag of the Words) method. It uses brute force matching of ORB features.
+        As we tested, DBOW is good for reducing computing resource, but not good for localization accuaracy.
+    """
 
     def __init__(
         self,
@@ -31,7 +34,7 @@ class LocalizationOrbMatching(LocalizationBase):
 
     def _generate_ORB_feature_database(self):
         """Initialize ORB instance & get ORB local features from images."""
-        # Make list to iterate
+        # Make image list to iterate
         map_obs_file_list = [self.map_obs_dir + os.sep + file for file in self.sorted_map_obs_file]
         query_list = [self.query_dir + os.sep + file for file in self.sorted_query_file]
 
@@ -50,20 +53,23 @@ class LocalizationOrbMatching(LocalizationBase):
         self.bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
         start = time.time()
-        desc_db = []
         num_nonetype = 0
 
+        # Extract local descriptors(ORB) from all map images, and store them as database
+        desc_map = []
         for i in range(0, len(map_obs_file_list), self.num_frames_per_node):
+            # Concatenate images if multiple images are assinged to a single node
             frame_list = []
             for k in range(self.num_frames_per_node):
                 frame_list.append(cv2.imread(map_obs_file_list[i + k]))
-            db_image = np.concatenate(frame_list, axis=1)
-            _, db_des = self.orb.detectAndCompute(db_image, None)
-            if db_des is None:
-                db_des = np.zeros([1, 32], dtype=np.uint8)
+            map_image = np.concatenate(frame_list, axis=1)
+
+            _, map_des = self.orb.detectAndCompute(map_image, None)  # Extract local descriptor
+            if map_des is None:  # Error exception code for Nonetype result
+                map_des = np.zeros([1, 32], dtype=np.uint8)
                 num_nonetype = num_nonetype + 1
 
-            desc_db.append(db_des)
+            desc_map.append(map_des)  # Add current local descriptor to database(list)
 
         end = time.time()
         print("Number of NoneType in DB: ", num_nonetype)
@@ -71,21 +77,24 @@ class LocalizationOrbMatching(LocalizationBase):
 
         start = time.time()
         num_nonetype = 0
-        desc_query = []
 
+        # Extract local descriptors(ORB) from all query images, and store them as database
+        desc_query = []
         for query_obs_file in query_list:
             query_image = cv2.imread(query_obs_file)
-            _, query_des = self.orb.detectAndCompute(query_image, None)
-            if query_des is None:
+            _, query_des = self.orb.detectAndCompute(query_image, None)  # Extract local descriptor
+
+            if query_des is None:  # Error exception code for Nonetype result
                 query_des = np.zeros([1, 32], dtype=np.uint8)
                 num_nonetype = num_nonetype + 1
-            desc_query.append(query_des)
+
+            desc_query.append(query_des)  # Add current local descriptor to database(list)
 
         end = time.time()
         print("Number of NoneType in Query: ", num_nonetype)
         print("Query generation elapsed time: ", end - start)
 
-        return desc_db, desc_query
+        return desc_map, desc_query
 
     def localize_with_observation(self, query_id: str):
         """Get global localization result according to query id input."""
@@ -97,11 +106,14 @@ class LocalizationOrbMatching(LocalizationBase):
         for db_des in self.desc_db:
             matches = self.bf.match(self.desc_query[i], db_des)
             matches = sorted(matches, key=lambda x: x.distance)
-            matches = matches[:30]
+            matches = matches[:30]  # Use top 30 matches when calculating the score
 
             scores.append(sum([match.distance for match in matches]))
 
+        # Get the highest similarity node ID
         map_node_with_max_value = np.argmin(scores)
-        high_similarity_set = sorted(range(len(scores)), key=lambda i: scores[i])[:30]
+
+        # Get top 20 high similarity set
+        high_similarity_set = sorted(range(len(scores)), key=lambda i: scores[i])[:20]
 
         return map_node_with_max_value, high_similarity_set
